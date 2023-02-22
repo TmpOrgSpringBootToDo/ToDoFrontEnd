4cabbb202be30c2eb87646967dbcc24dfbd05ce2
ThanuraSilva <thanurasilva1989@gmail.com>
2023-02-22 16:04:28 +0530

    State managemant on process -1 and prev commit is *Basic..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
+import { useState } from 'react';
 import './App.css';
 import { MainForm } from './components/MainForm';
 
+
 function App() {
+  const [task, setTask] = useState ([]);
+  
+  const addTask = (task) =>{
+    setTask(pvState => [...pvState, task])
+    //console.log(task)
+  }
+
   return (
     <div className="App">
       <h1>ToDoView -v 1.0.0</h1>
       <p>For Better Planed Solutions</p>
-      <MainForm/>
+      <MainForm addTask= {addTask} />
     </div>
   );
 }
--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PlayIcon } from '@heroicons/react/24/solid'
 
-export const MainForm = () => {
+
+export const MainForm = ({addTask}) => {
+    const [mainInput,setMainInput] = useState("")
 
   const handleFormSubmit = (e) =>{
      e.preventDefault();
-     console.log(e);
+
+     //Get input values to out side
+     addTask({
+         name:mainInput,
+         checked:false,
+         id:Date.now()
+     });
+
+
+
+
+     setMainInput("");
+    // console.log(e);
   }
 
-  return (
+  return (    
     <form
     className='todoForm'
     onSubmit={handleFormSubmit}
+    
     >
+        <p>{mainInput}</p>
         <div className='outer-boundry'>
            <input
            type="text"
-           id="main-input"
-           //onInput={(e) => setTask(e.target.value)}
+           id="mainInput"
+           value={mainInput}
+           onInput={(e) => setMainInput(e.target.value)}
+           required
            autoFocus
            maxLength={100}
            placeholder ="Add Your Tasks Here"
-           />
-           
+           />          
         </div>
+        <button
+        className='btn-btn primary'
+        aria-label='Add Task'
+        type='submit'       
+        >
+        <PlayIcon className="h-6 w-6 text-blue-500"/>
+         Add 
+        </button>
 
     </form>
   )
