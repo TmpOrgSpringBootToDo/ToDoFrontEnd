e4d2e1fb76ef17467ee1e87d0c9526e4a62992cd
ThanuraSilva <thanurasilva1989@gmail.com>
2023-02-23 20:58:47 +0530

    Connect with BackEnd - 2

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,52 +1,79 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import Axios from "axios";
 
 
-import { PlusIcon } from '@heroicons/react/24/solid'
+import {PlusIcon} from '@heroicons/react/24/solid'
 
-const MainForm = ({ addTask }) => {
-  const [task, setTask] = useState("");
+const MainForm = ({addTask}) => {
+    const [task, setTask] = useState("");
 
-  const handleFormSubmit = (e) => {
+    const handleFormSubmit = (e) => {
 
-    e.preventDefault();
-    addTask({
-      name: task,
-      checked: false,
-      id: Date.now()
-    })
-    setTask("")
-  }
+        e.preventDefault();
+        const todoTask = {
+            name: task,
+            checked: false,
+            id: Date.now()
+        }
+        addTask(todoTask)
+        setTask("")
+        callTodoAPI(todoTask)
+    }
 
-  return (
-    <form
-      className="todo"
-      onSubmit={handleFormSubmit}
-      >
-      <div className="wrapper">
-        <input
-          type="text"
-          id="task"
-          className="input"
-          value={task}
-          onInput={(e) => setTask(e.target.value)}
-          required
-          autoFocus
-          maxLength={60}
-          placeholder="Enter Task"
-        />
-        <label
-          htmlFor="task"
-          className="label"
-        >ADD YOUR TASK</label>
-      </div>
-      <button
-        className="btn"
-        aria-label="Add Task"
-        type="submit"
+    /**
+     * {
+     *     "userIdTodo":"d88c65bc-e2b2-4039-bf55-801bfda0dd90",
+     *     "toDo":"mytodo",
+     *     "dateTime": "{{currentDate}}"
+     * }
+     * @param todoTask
+     */
+    const callTodoAPI = (todoTask) => {
+        const data = {
+            userIdTodo: "d88c65bc-e2b2-4039-bf55-801bfda0dd90",
+            toDo: todoTask.name,
+            dateTime: new Date()
+        }
+        console.log(`todoTaskData: ${JSON.stringify(data)}`)
+        Axios.post("http://localhost:8080/todo/api/v1/users/d88c65bc-e2b2-4039-bf55-801bfda0dd90/todo", data)
+            .then(res => {
+                console.log(`Response: ${JSON.stringify((res))}`)
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    return (
+        <form
+            className="todo"
+            onSubmit={handleFormSubmit}
         >
-        <PlusIcon />
-      </button>
-    </form>
-  )
+            <div className="wrapper">
+                <input
+                    type="text"
+                    id="task"
+                    className="input"
+                    value={task}
+                    onInput={(e) => setTask(e.target.value)}
+                    required
+                    autoFocus
+                    maxLength={60}
+                    placeholder="Enter Task"
+                />
+                <label
+                    htmlFor="task"
+                    className="label"
+                >ADD YOUR TASK</label>
+            </div>
+            <button
+                className="btn"
+                aria-label="Add Task"
+                type="submit"
+            >
+                <PlusIcon/>
+            </button>
+        </form>
+    )
 }
 export default MainForm
